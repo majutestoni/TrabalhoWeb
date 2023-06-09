4007e84cb101221aa5af9259f5ae192b21efeba1
majutestoni <majuliatestoni@gmail.com>
2023-06-09 10:52:53 -0300

    service

--- src/app/components/funcionarios/funcionarios.module.ts
+++ src/app/components/funcionarios/funcionarios.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FuncionariosComponent } from './funcionarios.component';
 import { FuncionariosRoutingModule } from './funcionarios-routing.module';
+import { CardModule } from 'src/app/shared/card/card.module';
+import { FuncionariosService } from './funcionarios.service';
 
 @NgModule({
   declarations: [FuncionariosComponent],
-  imports: [CommonModule, FuncionariosRoutingModule],
+  providers: [FuncionariosService],
+  imports: [CommonModule, FuncionariosRoutingModule, CardModule],
 })
 export class FuncionariosModule {}
