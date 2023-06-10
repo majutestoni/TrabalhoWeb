f8d38b58d48fac500e60e5b533353fafc4cbcd7f
majutestoni <majuliatestoni@gmail.com>
2023-06-10 15:19:50 -0300

    ajustes

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, MenuComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, FooterComponent, MenuComponent],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class ComponentsModule {}
