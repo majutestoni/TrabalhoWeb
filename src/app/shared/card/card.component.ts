94f8fdad9db9b06b0fd688e59020eb6e403111f6
Lucas Queiroz Rocha dos Reis <lqrreis@furb.br>
2023-06-10 15:50:39 -0300

    Componente de redirecionamento de email

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -3,14 +3,17 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  @Input() linkFoto = '';
+  @Input() nome = '';
+  @Input() propriedade1 = '';
+  @Input() propriedade2 = '';
+  @Input() email = '';
 
-  @Input() linkFoto = ''
-  @Input() nome = ''
-  @Input() propriedade1 = ''
-  @Input() propriedade2 = ''
-  @Input() email = ''
-
+  redirectToOutlook(): void {
+    const email = 'seuemail@dominio.com';
+    window.location.href = `https://outlook.live.com/owa/?path=/mail/action/compose&to=${email}`;
+  }
 }
