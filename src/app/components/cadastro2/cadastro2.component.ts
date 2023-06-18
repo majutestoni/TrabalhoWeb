5c9145810594b15e352eafa2553d4d2ebb5f2060
majutestoni <majuliatestoni@gmail.com>
2023-06-18 15:09:16 -0300

    iniciando ajustes

---
+++ src/app/components/cadastro2/cadastro2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro2',
+  templateUrl: './cadastro2.component.html',
+  styleUrls: ['./cadastro2.component.scss']
+})
+export class Cadastro2Component {
+
+}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { Cadastro2Component } from './cadastro2/cadastro2.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, Cadastro2Component],
   imports: [CommonModule],
   exports: [HeaderComponent, FooterComponent],
 })
